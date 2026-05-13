ce7c57156063e86e33ec97f8fd551318fc3a5193
rechazaloultimo <pizarro.rodrigoariel@gmail.com>
2026-05-13 16:44:28 -0300

    act

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  base: "/romiweb/",
   vite: {
     ssr: {
       external: ["gsap", "three"],
